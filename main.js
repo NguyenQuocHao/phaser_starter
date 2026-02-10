3282276c43b82d614a6b30be7725ee430eea008c
NguyenQuocHao <nguyenquochao2000@gmail.com>
2026-02-10 00:33:03 -0500

    replace absolute paths

--- main.js
+++ main.js
@@ -8,13 +8,11 @@ const sizes = {
 
 const speedDown = 300;
 
-const gameStartDiv = document.querySelector("#gameStartDiv")
-const gameStartBtn = document.querySelector("#gameStartBtn")
-const gameEndDiv = document.querySelector("#gameEndDiv")
-const gameWinLoseSpan = document.querySelector("#gameWinLoseSpan")
-const gameEndScoreSpan = document.querySelector("#gameEndScoreSpan")
-
-
+const gameStartDiv = document.querySelector("#gameStartDiv");
+const gameStartBtn = document.querySelector("#gameStartBtn");
+const gameEndDiv = document.querySelector("#gameEndDiv");
+const gameWinLoseSpan = document.querySelector("#gameWinLoseSpan");
+const gameEndScoreSpan = document.querySelector("#gameEndScoreSpan");
 
 class GameScene extends Phaser.Scene {
   constructor() {
@@ -23,34 +21,34 @@ class GameScene extends Phaser.Scene {
     this.cursor;
     this.playerSpeed = speedDown + 50;
     this.target;
-    this.points = 0
-    this.textScore
-    this.textTime
-    this.timedEvent
-    this.remainingTime
-    this.coinMusic
-    this.bgMusic
-    this.emitter
+    this.points = 0;
+    this.textScore;
+    this.textTime;
+    this.timedEvent;
+    this.remainingTime;
+    this.coinMusic;
+    this.bgMusic;
+    this.emitter;
   }
 
   preload() {
-    this.load.image("bg", "/assets/bg.png");
-    this.load.image("basket", "/assets/basket.png");
-    this.load.image("apple", "/assets/apple.png");
-    this.load.image("money", "/assets/money.png");
-    this.load.audio("coin", "/assets/coin.mp3");
-    this.load.audio("bgMusic", "/assets/bgMusic.mp3");
+    const base = import.meta.env.BASE_URL;
+
+    this.load.image("bg", `${base}assets/bg.png`);
+    this.load.image("basket", `${base}assets/basket.png`);
+    this.load.image("apple", `${base}assets/apple.png`);
+    this.load.image("money", `${base}assets/money.png`);
+    this.load.audio("coin", `${base}assets/coin.mp3`);
+    this.load.audio("bgMusic", `${base}assets/bgMusic.mp3`);
   }
 
   create() {
+    this.scene.pause("scene-game");
 
-    this.scene.pause("scene-game")
-
-    this.coinMusic = this.sound.add("coin")
-    this.bgMusic=this.sound.add("bgMusic")
-    this.bgMusic.play()
+    this.coinMusic = this.sound.add("coin");
+    this.bgMusic = this.sound.add("bgMusic");
+    this.bgMusic.play();
     // this.bgMusic.stop()
-
 
     this.add.image(0, 0, "bg").setOrigin(0, 0);
     this.player = this.physics.add
@@ -59,12 +57,26 @@ class GameScene extends Phaser.Scene {
     this.player.setImmovable(true);
     this.player.body.allowGravity = false;
     this.player.setCollideWorldBounds(true);
-    this.player.setSize(this.player.width-this.player.width/4, this.player.height/6).setOffset(this.player.width/10, this.player.height - this.player.height/10);
+    this.player
+      .setSize(
+        this.player.width - this.player.width / 4,
+        this.player.height / 6,
+      )
+      .setOffset(
+        this.player.width / 10,
+        this.player.height - this.player.height / 10,
+      );
 
     this.target = this.physics.add.image(0, 0, "apple").setOrigin(0, 0);
     this.target.setMaxVelocity(0, speedDown);
 
-    this.physics.add.overlap(this.target,this.player,this.targetHit, null, this)
+    this.physics.add.overlap(
+      this.target,
+      this.player,
+      this.targetHit,
+      null,
+      this,
+    );
 
     this.cursor = this.input.keyboard.createCursorKeys();
 
@@ -77,27 +89,32 @@ class GameScene extends Phaser.Scene {
       fill: "#000000",
     });
 
-    this.timedEvent = this.time.delayedCall(30000,this.gameOver,[], this)
+    this.timedEvent = this.time.delayedCall(30000, this.gameOver, [], this);
 
-    this.emitter=this.add.particles(0,0,"money",{
-      speed:100,
-      gravityY:speedDown-200,
-      scale:0.04,
-      duration:100,
-      emitting:false
-    })
-    this.emitter.startFollow(this.player, this.player.width / 2, this.player.height / 2,true);
-
+    this.emitter = this.add.particles(0, 0, "money", {
+      speed: 100,
+      gravityY: speedDown - 200,
+      scale: 0.04,
+      duration: 100,
+      emitting: false,
+    });
+    this.emitter.startFollow(
+      this.player,
+      this.player.width / 2,
+      this.player.height / 2,
+      true,
+    );
   }
 
   update() {
-    this.remainingTime=this.timedEvent.getRemainingSeconds()
-    this.textTime.setText(`Remaining Time: ${Math.round(this.remainingTime).toString()}`)
-
+    this.remainingTime = this.timedEvent.getRemainingSeconds();
+    this.textTime.setText(
+      `Remaining Time: ${Math.round(this.remainingTime).toString()}`,
+    );
 
     if (this.target.y >= sizes.height) {
       this.target.setY(0);
-      this.target.setX(this.getRandomX())
+      this.target.setX(this.getRandomX());
     }
 
     const { left, right } = this.cursor;
@@ -116,28 +133,26 @@ class GameScene extends Phaser.Scene {
   }
 
   targetHit() {
-    this.coinMusic.play()
-    this.emitter.start()
+    this.coinMusic.play();
+    this.emitter.start();
     this.target.setY(0);
     this.target.setX(this.getRandomX());
     this.points++;
-    this.textScore.setText(`Score: ${this.points}`)
+    this.textScore.setText(`Score: ${this.points}`);
   }
 
-  gameOver(){
-    this.sys.game.destroy(true)
-    if(this.points >=10){
-      gameEndScoreSpan.textContent = this.points
-      gameWinLoseSpan.textContent= "Win! 😊"
-
-    }else{
-      gameEndScoreSpan.textContent = this.points
-      gameWinLoseSpan.textContent= "Lose! 😭"
+  gameOver() {
+    this.sys.game.destroy(true);
+    if (this.points >= 10) {
+      gameEndScoreSpan.textContent = this.points;
+      gameWinLoseSpan.textContent = "Win! 😊";
+    } else {
+      gameEndScoreSpan.textContent = this.points;
+      gameWinLoseSpan.textContent = "Lose! 😭";
     }
 
-    gameEndDiv.style.display="flex"
+    gameEndDiv.style.display = "flex";
   }
-
 }
 
 const config = {
@@ -157,7 +172,7 @@ const config = {
 
 const game = new Phaser.Game(config);
 
-gameStartBtn.addEventListener("click", ()=>{
-  gameStartDiv.style.display="none"
-  game.scene.resume("scene-game")
-})
+gameStartBtn.addEventListener("click", () => {
+  gameStartDiv.style.display = "none";
+  game.scene.resume("scene-game");
+});
